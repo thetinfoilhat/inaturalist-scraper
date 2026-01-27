cb05f7d68c94123950eaeececb796bdf2d7a8028
Aiden Xie <adrianxie00@gmail.com>
2026-01-27 12:19:38 -0600

    specimen questions schema

---
+++ sql/specimen_questions.sql
@@ -0,0 +1,26 @@
+ CREATE TABLE public.specimen_questions (
+    id UUID NOT NULL,
+    question STRING NOT NULL,
+    tournament STRING NOT NULL,
+    division STRING NOT NULL,
+    options JSONB NULL DEFAULT '[]':::JSONB,
+    answers JSONB NOT NULL,
+    subtopics JSONB NULL DEFAULT '[]':::JSONB,
+    difficulty DECIMAL NULL DEFAULT 0.5:::DECIMAL,
+    event STRING NOT NULL,
+    images JSONB NOT NULL DEFAULT '[]':::JSONB,
+    random_f FLOAT8 NULL DEFAULT random(),
+    created_at TIMESTAMPTZ NULL DEFAULT now():::TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ NULL DEFAULT now():::TIMESTAMPTZ,
+    question_type STRING NULL AS (
+      CASE
+        WHEN (jsonb_typeof(options) = 'array':::STRING)
+         AND (jsonb_array_length(options) >= 2:::INT8)
+        THEN 'mcq':::STRING
+        ELSE 'frq':::STRING
+      END
+    ) STORED,
+    pure_id BOOL NULL DEFAULT false,
+    rm_type STRING NULL,
+    specimen STRING NOT NULL
+  );
